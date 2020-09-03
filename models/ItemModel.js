efb243ff06455697c138fb2a400d020e22503b85
rdj519 <39096856+rdj519@users.noreply.github.com>
2020-09-03 17:47:35 +0800

    Add files via upload

---
+++ models/ItemModel.js
@@ -0,0 +1,38 @@
+var mongoose = require('mongoose');
+
+
+//defines the schema for collection 'items'
+var ItemSchema = new mongoose.schema({
+    // name of item
+    name: {
+        type: String,
+        required: true
+    },
+    // amount per pack
+    amount: {
+        type: Number,
+        required: true
+    },
+    // price per pack
+    price: {
+        type: Number,
+        required: true
+    },
+    // quantity of packs
+    quantity: {
+        type: Number,
+        required: true
+    },
+    // low stock quantity threshold (for notifications)
+    lowStockQuantity: {
+        type: Number,
+        required: true
+    }
+});
+
+
+// exports a mongoose.model object based on `ItemSchema` (defined above)
+// when another script exports from this file
+// This model executes CRUD operations
+// to collection `item` -> plural of the argument `Item`
+module.exports = mongoose.model('Item', ItemSchema);
