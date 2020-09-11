3c6c5203aa4eab0c24f358bc02f208536b991e7f
rdj519 <39096856+rdj519@users.noreply.github.com>
2020-09-11 23:27:13 +0800

    Add files via upload

--- controllers/ordersController.js
+++ controllers/ordersController.js
@@ -4,11 +4,6 @@ const db = require('../models/db.js');
 const Order = require('../models/OrderModel.js');
 
 const ordersController = {
-    getOrders: function(req,res){
-        res.render('orders', {
-            title:  'Manage Orders',
-        });
-    },
 
     getOrders: function (req, res) {
 
@@ -50,7 +45,112 @@ const ordersController = {
                 res.render('error');
             }
         }, mySort);
+    },
+
+    getOrdersByPaymentMethod: function (req, res) {
+
+        var query = {};
+        var projection = {};
+        var mySort = {paymentMethod: 1}
+
+
+        db.findMany(Order, query, projection, function(result) {
+
+            if(result != null) {
+
+                res.render('search', {contents: result});
+            }
+
+            else {
+
+                res.render('error');
+            }
+        }, mySort);
+    },
+
+    getOrdersByCourier: function (req, res) {
+
+        var query = {};
+        var projection = {};
+        var mySort = {courier: 1}
+
+
+        db.findMany(Order, query, projection, function(result) {
+
+            if(result != null) {
+
+                res.render('search', {contents: result});
+            }
+
+            else {
+
+                res.render('error');
+            }
+        }, mySort);
+    },
+    getOrdersByCity: function (req, res) {
+
+        var query = {};
+        var projection = {};
+        var mySort = {city: 1}
+
+
+        db.findMany(Order, query, projection, function(result) {
+
+            if(result != null) {
+
+                res.render('search', {contents: result});
+            }
+
+            else {
+
+                res.render('error');
+            }
+        }, mySort);
+    },
+
+    getOrdersByPlacedDate: function (req, res) {
+
+        var query = {};
+        var projection = {};
+        var mySort = {placedDate: 1}
+
+
+        db.findMany(Order, query, projection, function(result) {
+
+            if(result != null) {
+
+                res.render('search', {contents: result});
+            }
+
+            else {
+
+                res.render('error');
+            }
+        }, mySort);
+    },
+
+    getOrdersByDeliveryDate: function (req, res) {
+
+        var query = {};
+        var projection = {};
+        var mySort = {deliveryDate: 1}
+
+
+        db.findMany(Order, query, projection, function(result) {
+
+            if(result != null) {
+
+                res.render('search', {contents: result});
+            }
+
+            else {
+            	
+                res.render('error');
+            }
+        }, mySort);
     }
+
 }
 
 module.exports = ordersController;
