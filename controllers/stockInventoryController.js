4cb79f4951a7b33ba733d645b010ea2a595de026
rdj519 <39096856+rdj519@users.noreply.github.com>
2020-09-29 17:32:50 +0800

    Add files via upload

--- controllers/stockInventoryController.js
+++ controllers/stockInventoryController.js
@@ -14,11 +14,11 @@ const stockInventoryController = {
     },
 
     getProductQuantity: function(req, res){
-        var query = {name: req.query.name};
+        var query = {_id: req.query._id};
         var projection = {};
 
         db.findOne(Product, query, projection, function(result){
-            res.send(result.quantity);
+            res.send(result);
         });
     },
 
@@ -34,12 +34,11 @@ const stockInventoryController = {
     getPlaceStockOrder: function(req, res) {
        var query = {_id: req.query.productID}
        var decrement = req.query.quantity
-        console.log("productID: " + req.query.productID);
-       console.log("getPlaceStockOrder\n");
+
        db.findOne(Product, query, null, function(result) {
-     
-            db.updateOne(Product,query, {quantity:decrement}, function(results) {
-             console.log("results: " + results);
+            
+            db.updateOne(Product,query, {$inc:{quantity:-decrement}}, function(results) {
+             console.log("results (product): " + results);
             });
        });
 
